b86f151a67ed6aecb26e6ab4eb084a68b9b60c8d
Roger Silva <karlorms@gmail.com>
2022-01-12 14:15:54 -0300

    Pagination layout

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,9 +1,10 @@
+import Pagination from "components/Pagination";
 import exp from "constants"
 
 function Listing() {
 
     return(
-        <h1>Página de listagem</h1>
+        <Pagination/>
     )
 }
 
