fef6a97eb6f8b7233a59a98511524cece6cd3eee
Roger Silva <karlorms@gmail.com>
2022-01-16 02:02:29 -0300

    useParams

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -7,7 +7,7 @@ function Navbar() {
         <header>
             <nav className="container">
                 <div className="dsmovie-nav-content">
-                    <h1>DSMovie</h1>
+                    <h1>ROGMovie</h1>
                     <a href="https://github.com/karlorms">
                         <div className="dsmovie-contact-container">
                             <GithubIcon />
